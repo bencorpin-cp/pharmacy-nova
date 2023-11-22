810c44ce3d2df9fe1f1288e88a480ca283e904bc
Ben Corpin <ben.corpin@coreproc.com>
2023-11-22 18:19:53 +0800

    pharmacy_id foreign added to employees table

---
+++ database/migrations/2023_11_22_100212_add_pharmacy_id_to_employees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Pharmacy::class)
+                ->after("id")
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropForeign("pharmacy_id");
+        });
+    }
+};
