b596f66bbd729c9c20b95ddec051eeb448e25c2a
Ben Corpin <ben.corpin@coreproc.com>
2023-11-22 16:36:27 +0800

    initial commit

---
+++ database/migrations/2023_11_22_080046_create_contracts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\DrugManufacturer::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Pharmacy::class)->constrained()->cascadeOnDelete();
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
