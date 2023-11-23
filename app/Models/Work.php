35b2e2a429e5be040db6f7ce31d97c03d63e8512
Ben Corpin <ben.corpin@coreproc.com>
2023-11-23 15:36:50 +0800

    Work model and resource created

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Work extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        "shift_start" => "datetime",
+        "shift_end" => "datetime",
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function pharmacy()
+    {
+        return $this->belongsTo(Pharmacy::class);
+    }
 }
