3350591a2ec9f9ec1833767a7b534b26817886da
Ben Corpin <ben.corpin@coreproc.com>
2023-11-24 15:10:18 +0800

    customize the menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,7 +2,21 @@
 
 namespace App\Providers;
 
+use App\Nova\Contract;
+use App\Nova\Dashboards\Main;
+use App\Nova\Doctor;
+use App\Nova\Drug;
+use App\Nova\DrugManufacturer;
+use App\Nova\Employee;
+use App\Nova\Patient;
+use App\Nova\Pharmacy;
+use App\Nova\Prescription;
+use App\Nova\Seen;
+use App\Nova\Work;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -18,6 +32,47 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
         parent::boot();
 
         Nova::withBreadcrumbs();
+
+        Nova::mainMenu(function (Request $request){
+            return [
+                MenuSection::dashboard(Main::class)->icon("chart-bar"),
+
+                MenuSection::make("Doctors", [
+                    MenuItem::resource(Doctor::class),
+                    MenuItem::resource(Seen::class),
+                ])
+                    ->collapsable()
+                    ->icon("user"),
+
+                MenuSection::make("Patients", [
+                    MenuItem::resource(Patient::class),
+                    MenuItem::resource(Prescription::class),
+                ])
+                    ->collapsable()
+                    ->icon("user"),
+
+                MenuSection::make("Pharmacies", [
+                    MenuItem::resource(Pharmacy::class),
+                    MenuItem::resource(Contract::class),
+                ])
+                    ->collapsable()
+                    ->icon("home"),
+
+                MenuSection::make("Drugs", [
+                    MenuItem::resource(Drug::class),
+                    MenuItem::resource(DrugManufacturer::class),
+                ])
+                    ->collapsable()
+                    ->icon("plus-circle"),
+
+                MenuSection::make("Employees", [
+                    MenuItem::resource(Employee::class),
+                    MenuItem::resource(Work::class),
+                ])
+                    ->collapsable()
+                    ->icon("user-group"),
+            ];
+        });
     }
 
     /**
